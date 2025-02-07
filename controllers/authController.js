6ef1c136972d338bf6109e6f39e5fa57f83a5ecd
owalkz <jjaraorie@gmail.com>
2025-02-07 15:03:02 +0300

    Added Forgot and Reset Password functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,7 +20,7 @@ const register = async (req, res, next) => {
   try {
     const emailExists = await User.findOne({ emailAddress });
     if (emailExists) {
-      return res.status(400).json({ message: "Email already exists" });
+      return res.status(400).json({ message: "EmailAddress already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
@@ -40,7 +40,7 @@ const register = async (req, res, next) => {
       emailContent
     );
     if (!emailSent) {
-      return res.status(400).json({ message: "Invalid email" });
+      return res.status(400).json({ message: "Invalid emailAddress" });
     }
     if (!user) {
       return res
@@ -64,25 +64,84 @@ const login = async (req, res, next) => {
     if (!user) {
       return res.status(422).json({ message: "Invalid credentials entered" });
     }
-    if (user.matchPassword(password)) {
+    const isMatch = await user.matchPassword(password);
+    if (isMatch) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
         expiresIn: "1d",
       });
-      const {password: _, ...userWithoutPassword} = user.toObject();
-      res.status(200).json({
+      const { password: _, ...userWithoutPassword } = user.toObject();
+      return res.status(200).json({
         message: "Login successful!",
         token: token,
         user: userWithoutPassword,
       });
+    } else {
+      return res.status(422).json({ message: "Invalid credentials entered" });
     }
   } catch (err) {
     return res.status(500).json({ message: "Failed to login." });
   }
 };
 
-const forgotPassword = (req, res, next) => {};
+const forgotPassword = async (req, res, next) => {
+  const { emailAddress } = req.body;
+  if (!emailAddress) {
+    return res.status(400).json({ message: "All fields are required!" });
+  }
+  const user = await User.findOne({ emailAddress });
+  if (!user) {
+    return res.status(404).json({ message: "User not found!" });
+  }
+  const resetToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "10m",
+  });
+  console.log(resetToken);
+  const resetURL = `${process.env.CLIENT_URL}/auth/reset-password/${resetToken}`;
+  const filePath = path.join(
+    __dirname,
+    "../utils/mailingAssets/resetPassword.html"
+  );
+  const templateString = fs.readFileSync(filePath, "utf8");
+  const emailContent = templateString
+    .replace("${name}", user.first_name)
+    .replace("${resetLink}", resetURL);
+  const emailSent = await sendEmail(
+    emailAddress,
+    "Password Reset Request",
+    emailContent
+  );
+  if (!emailSent) {
+    return res.status(400).json({ message: "Invalid email" });
+  }
+  return res
+    .status(200)
+    .json({ message: "Password reset email sent successfully" });
+};
 
-const resetPassword = (req, res, next) => {};
+const resetPassword = async (req, res, next) => {
+  const resetToken = req.params.id;
+  const { password } = req.body;
+  try {
+    const decoded = jwt.verify(resetToken, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Password reset token is invalid or has expired" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.password = hashedPassword;
+    try {
+      await user.save();
+    } catch (saveError) {
+      console.error("Error saving user:", saveError);
+    }
+    res.status(200).json({ message: "Password has been reset" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 module.exports = {
   register,
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,13 +3,11 @@ const router = express.Router();
 const {
   register,
   login,
-  logout,
   resetPassword,
   forgotPassword,
 } = require("../controllers/authController");
 router.post("/register", register);
 router.post("/login", login);
-// router.post("/logout", logout);
-// router.post("/forgot-password", forgotPassword);
-// router.post("/reset-password/:id", resetPassword);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:id", resetPassword);
 module.exports = router;
